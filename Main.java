7b4a7bfcbfb656d8a4f3ae0bf483922be2b47b2e
bernborgess <bernborgess@outlook.com>
2022-11-23 17:09:24 -0300

    Commit 1

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ *  Engenharia de Software
+ *  Refactoring
+ *  Bernardo Nogueira Borges
+ *  2020006396
+ */
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("GO!");
+  }
+}
