a0313dca9e404997e1c07c9663e0117ae067d7de
bernborgess <bernborgess@outlook.com>
2022-11-24 12:11:12 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -36,19 +36,7 @@ public class Movie {
   }
 
   public double getCharge(int daysRented) {
-    switch (getPriceCode()) {
-      case Movie.REGULAR:
-        return (daysRented > 2) ? (daysRented - 2) * 1.5 + 2
-            : 2;
-
-      case Movie.NEW_RELEASE:
-        return daysRented * 3;
-
-      case Movie.CHILDRENS:
-        return (daysRented > 3) ? (daysRented - 3) * 1.5 + 1.5
-            : 1.5;
-    }
-    return 0.0;
+    return _price.getCharge(daysRented);
   }
 
   public int getFrequentRenterPoints(int daysRented) {
