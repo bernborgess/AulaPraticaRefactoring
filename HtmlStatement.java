87a76b2caf301fa19370f20c0f5f345ad5a6ead7
bernborgess <bernborgess@outlook.com>
2022-11-24 12:52:15 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,30 +1,16 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
 
-  public String value(Customer aCustomer) {
-    Enumeration<Rental> rentals = aCustomer.getRentals();
-    String result = getHeader(aCustomer);
-    while (rentals.hasMoreElements()) {
-      Rental each = rentals.nextElement();
-      result += getRentalContent(each);
-    }
-
-    result += getFooter(aCustomer);
-    return result;
-  }
-
-  private String getHeader(Customer aCustomer) {
+  protected String getHeader(Customer aCustomer) {
     return "<h1>Rentals for <em>" + aCustomer.getName()
         + "</em><h1><p>\n";
   }
 
-  private String getRentalContent(Rental aRental) {
+  protected String getRentalContent(Rental aRental) {
     return aRental.getMovie().getTitle() + ": "
         + String.valueOf(aRental.getCharge()) + "<br>\n";
   }
 
-  private String getFooter(Customer aCustomer) {
+  protected String getFooter(Customer aCustomer) {
     return "<p>You owe <em>"
         + String.valueOf(aCustomer.getTotalCharge())
         + "</em><p>\n"
--- Statement.java
+++ Statement.java
@@ -1,3 +1,22 @@
-public class Statement {
+import java.util.Enumeration;
+
+public abstract class Statement {
+  protected abstract String getHeader(Customer aCustomer);
+
+  protected abstract String getRentalContent(Rental aRental);
+
+  protected abstract String getFooter(Customer aCustomer);
+
+  public String value(Customer aCustomer) {
+    Enumeration<Rental> rentals = aCustomer.getRentals();
+    String result = getHeader(aCustomer);
+    while (rentals.hasMoreElements()) {
+      Rental each = rentals.nextElement();
+      result += getRentalContent(each);
+    }
+
+    result += getFooter(aCustomer);
+    return result;
+  }
 
 }
